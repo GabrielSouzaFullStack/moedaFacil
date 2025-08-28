b075c7445e7a2a8272e4bfcc04a2e8e641cd1bf1
GabrielSouzaFullStack <gabrieldesouza@cesurg.com>
2025-08-28 20:19:12 -0300

    :fire: Remove index.html and script.js files

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-async function renderizarCotacao() {
-  await fetch("index.php")
-    .then((res) => res.text())
-    .then((data) => {
-      document.getElementById("cotacao").innerHTML = data;
-    });
-}
-
-renderizarCotacao();
